2aa8edfcf24fc971f0b4b4c8f3b028855883e7f7
suman <suman.kumar235@gmail.com>
2019-04-01 01:32:14 +0530

    my changes

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent{
+export class RegisterComponent implements OnInit{
 form:any;
-  constructor() { }
-  registerSubmit(form){
-    console.log(form.value);
+registerForm: FormGroup;
+  registerSubmit(){
+    if(this.registerForm.valid){
+      console.log(this.registerForm.value);
+    }
+    }
+    ngOnInit(){
+      this.registerForm=new FormGroup({
+        'firstName': new FormControl(null, Validators.required),
+        'lastname': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'password': new FormControl(null, Validators.required),
+        'confirmpassword': new FormControl(null, Validators.required) 
+      });
     }
 }
